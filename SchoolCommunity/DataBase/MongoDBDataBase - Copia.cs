c088183d0008fc521e9570a4b54ff27f95f10a0e
asprodrigosouza <tenrodrigo@yahoo.com.br>
2022-02-18 12:41:05 -0300

    d
    d

---
+++ SchoolCommunity/DataBase/MongoDBDataBase - Copia.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolCommunity.DataBase
+{
+    public class MongoDBDataBase
+    {
+        
+        public IMongoDatabase GetConnect()
+        {
+            // Conectar no Servidor
+            Console.WriteLine("Conectando no Servidor...");
+            var client = new MongoClient("mongodb://localhost:27017");
+            
+            // Conectar no Banco de Dados
+            Console.WriteLine("Conectando no Bando de Dados...");
+            var dataBaseConn = client.GetDatabase("SchoolCommunityDB");
+
+            // Retorna Conexão Banco
+            return dataBaseConn;
+        }
+        
+    }
+}
---
+++ SchoolCommunity/DataBase/MongoDBDataBase.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolCommunity.DataBase
+{
+    public sealed class MongoDBDataBase
+    {
+        private static volatile MongoDBDataBase instace;
+        private static object syncLock = new Object();
+        private static IMongoDatabase db = null;
+
+        private MongoDBDataBase()
+        {
+            Console.WriteLine("Conectando no Servidor...");
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            Console.WriteLine("Conectando no Bando de Dados...");
+            db = client.GetDatabase("SchoolCommunityDB");
+        }
+
+
+        public static IMongoDatabase GetConnect
+        {
+            get 
+            {
+                if (instace == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (instace == null)
+                        {
+                            instace = new MongoDBDataBase();
+                            Console.WriteLine("\n\n\n:: Nova Conexão - Instância Criada");
+                            
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n\n\n:: Reutilizando Conexão");
+                }
+
+                return db;
+            }
+        }
+        
+    }
+}
